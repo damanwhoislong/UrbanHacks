ac9fb5d014cd627135df93a3922d48fea65edd42
Longman Xu <longmanxu@live.ca>
2018-12-08 22:54:27 -0500

    indented properly
    use 4 spaces pls

--- js/find_nearby_POI.js
+++ js/find_nearby_POI.js
@@ -12,51 +12,51 @@ function getLocation() {
 
 // find all nearby POI
 function findNearby(coordLong, coordLat){
-    var info = [];  // name, long, lat of POI
-    var validLoc = [];  // all locations within radius
+	var info = [];  // name, long, lat of POI
+	var validLoc = [];  // all locations within radius
 
 	// juicy stuff
 	var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
+	xmlhttp.onreadystatechange = function () {
 		// 1 km radius to search
 		var r = 1;
 		// long and lat to km conversion
 		var longCov = 80.00;
-        var latCov = 111.045;
+		var latCov = 111.045;
 
-        if (this.readyState == 4 && this.status == 200) {
-            var jsonData = JSON.parse(this.responseText);
-            for (i = 0; i < jsonData.length; i++) {
-                // append long and lat to coord
+		if (this.readyState == 4 && this.status == 200) {
+			var jsonData = JSON.parse(this.responseText);
+			for (i = 0; i < jsonData.length; i++) {
+				// append long and lat to coord
 				info.push({
 					'name': jsonData[i].TITLE,
-                    'x': jsonData[i].LONGITUDE,
-                    'y': jsonData[i].LATITUDE,
+					'x': jsonData[i].LONGITUDE,
+					'y': jsonData[i].LATITUDE,
 					'address': jsonData[i].ADDRESS,
 					'fax': jsonData[i].DESCRIPTION
-                });
-            }
+				});
+			}
 
-            // Initialize square range
-            // Scan thru and save all  points within square range
-            for (var i = 0; i < info.length; i++) {
-                if (info[i].x < coordLong + (r / longCov) &&
-                    info[i].x > coordLong - (r / longCov) &&
-                    info[i].y < coordLat + (r / latCov) &&
-                    info[i].y > coordLat - (r / latCov)) {
-                    // check if its within the circle
-										var dist = Math.sqrt(Math.pow((info[i].x - coordLong) * longCov, 2) + Math.pow((info[i].y - coordLat) * latCov, 2))
-                    if (dist < r) {
-                        validLoc.push({'name': info[i].name, 'dist': dist, 'address': info[i].address, 'fax': info[i].fax});
-                    }
-                }
-            }
-						validLoc.sort((a, b) => (a.dist > b.dist) ? 1 : ((b.dist > a.dist) ? -1 : 0));
+			// Initialize square range
+			// Scan thru and save all  points within square range
+			for (var i = 0; i < info.length; i++) {
+				if (info[i].x < coordLong + (r / longCov) &&
+				info[i].x > coordLong - (r / longCov) &&
+				info[i].y < coordLat + (r / latCov) &&
+				info[i].y > coordLat - (r / latCov)) {
+					// check if its within the circle
+					var dist = Math.sqrt(Math.pow((info[i].x - coordLong) * longCov, 2) + Math.pow((info[i].y - coordLat) * latCov, 2))
+					if (dist < r) {
+						validLoc.push({'name': info[i].name, 'dist': dist, 'address': info[i].address, 'fax': info[i].fax});
+					}
+				}
+			}
+			validLoc.sort((a, b) => (a.dist > b.dist) ? 1 : ((b.dist > a.dist) ? -1 : 0));
 		}
 
-    };
+	};
 	xmlhttp.open("GET", "https://damanwhoislong.github.io/UrbanHacks/Data/Tourism_Points_of_Interest.json", true);
-  xmlhttp.send();
+	xmlhttp.send();
 	console.log(validLoc);
 	return validLoc;
 }
